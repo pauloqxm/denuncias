4e189914fac29c974d4cb19e730ab137d0df15bf
pauloqxm <pauloqxm@gmail.com>
2025-08-06 18:30:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -54,6 +54,8 @@ st.markdown(f"""
         font-family: Tahoma, sans-serif;
         border-bottom: 3px solid #fad905;
         z-index: 9999;
+        text-align: center;
+        line-height: 1.4;
     }}
 
     .header-top {{
