df29428589f39ff5cc1ddba6226ca03a1db82740
pauloqxm <pauloqxm@gmail.com>
2025-08-06 18:36:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pandas as pd
 import folium
@@ -54,8 +55,6 @@ st.markdown(f"""
         font-family: Tahoma, sans-serif;
         border-bottom: 3px solid #fad905;
         z-index: 9999;
-        text-align: center;
-        line-height: 1.4;
     }}
 
     .header-top {{
