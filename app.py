b77b7624de1ee0c0a142e06f04f7e7087337ed42
pauloqxm <pauloqxm@gmail.com>
2025-08-06 22:14:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ st.markdown(f"""
         align-items: center;
         gap: 30px;
         flex-wrap: wrap;
-        margin-top: 0px;
+        margin-top: -12px;
         border-bottom: 2px solid #fad905;
     }}
 
