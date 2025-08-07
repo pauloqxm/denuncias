f0303f3fe5e0935b06df6b01db7b434dc6c0d47a
pauloqxm <pauloqxm@gmail.com>
2025-08-06 22:46:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ st.markdown(f"""
         gap: 30px;
         flex-wrap: wrap;
         margin-top: -40px;
-        border-bottom: 4px solid #b6b8ba;
+        border-bottom: 3px solid #b6b8ba;
     }}
 
     .social-menu a {{
